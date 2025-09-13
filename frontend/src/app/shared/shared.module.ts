ead69a432e6257cbe684a4de4219c0ed2386a2ec
João Pedro Lopes Moreno <jl.moreno7@hotmail.com>
2025-09-12 23:24:44 -0400

    feat: move CeilPipe to shared/utils for better modularity and organization

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -4,7 +4,7 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 import { BaseChartDirective } from 'ng2-charts';
-import { CeilPipe } from 'src/app/features/clients/client-list/ceil.pipe';
+import { CeilPipe } from 'src/app/shared/utils/ceil.pipe';
 
 @NgModule({
   imports: [
